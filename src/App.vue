ae7d8add02b38fa6575d0fb2f62d09d319d389a8
Juan Manuel <juanmanuel.tartaglia@gmail.com>
2020-02-07 10:56:34 -0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="player-container">
+      <video-player ref="videoPlayer" :options="playerOptions"> </video-player>
+    </div>
+    <div class="player-container">
+      <video-player ref="videoPlayer" :options="playerOptions"> </video-player>
+    </div>
+    <div class="player-container">
+      <video-player ref="videoPlayer" :options="playerOptions"> </video-player>
+    </div>
+    <div class="player-container">
+      <video-player ref="videoPlayer" :options="playerOptions"> </video-player>
+    </div>
+    <div class="player-container">
+      <video-player ref="videoPlayer" :options="playerOptions"> </video-player>
+    </div>
+    <div class="player-container">
+      <video-player ref="videoPlayer" :options="playerOptions"> </video-player>
+    </div>
+    <div class="player-container">
+      <video-player ref="videoPlayer" :options="playerOptions"> </video-player>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data: () => {
+    return {
+      sources: [
+        "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
+        "http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8",
+        "http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8"
+      ],
+      playerOptions: {
+        overNative: false,
+        autoplay: true,
+        controls: false,
+        width: 300,
+        height: 200,
+        techOrder: ["flash", "html5"],
+        sourceOrder: false,
+        html5: { hls: { withCredentials: false } },
+        sources: [
+          {
+            withCredentials: false,
+            type: "application/x-mpegURL",
+            src:
+              "http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8"
+          }
+        ]
+        // https://github.com/videojs/video.js/blob/master/docs/guides/options.md
+        // controlBar: {
+        //   timeDivider: false, // 时间分割线
+        //   durationDisplay: false, // 总时间
+        //   progressControl: true, // 进度条
+        //   customControlSpacer: true, // 未知
+        //   fullscreenToggle: true // 全屏
+        // },
+      }
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.player-container {
+  float: left;
+  width: 300;
+  height: 200;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import VueVideoPlayer from "vue-video-player";
+import "video.js/dist/video-js.css";
+import "vue-video-player/src/custom-theme.css";
+import "videojs-flash";
+import "videojs-contrib-hls/dist/videojs-contrib-hls";
+
+Vue.config.productionTip = false;
+
+Vue.use(VueVideoPlayer);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
